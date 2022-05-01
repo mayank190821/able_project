a09892f0589fd017941b93afa8cdd3d9c5ed8305
mayank190821 <bhugramayank@gmail.com>
2022-05-01 10:23:38 +0530

    deleted company model

--- job_opening/admin.py
+++ job_opening/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from job_opening.models import Companie
+from job_opening.models import NewJob
 # Register your models here.
 admin.site.site_header = "Able Jobs Admin"
 admin.site.site_title = "Able Jobs Admin Portal"
 admin.site.index_title = "Welcome to Able Jobs"
-admin.site.register(Companie)
+admin.site.register(NewJob)

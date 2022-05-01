c4409aa4ba4f98bda226e8903f4b2780ae0c495f
mayank190821 <bhugramayank@gmail.com>
2022-05-01 13:21:01 +0530

    added regex validation

--- job_opening/views.py
+++ job_opening/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User,auth
 from job_opening.models import NewJob
 from django.contrib.auth import logout
 from django.contrib import messages
+import re
 # username admin password admin123@
 
 def base(request):
@@ -25,17 +26,23 @@ def register(request):
         email = request.POST.get('registerEmail')
         password = request.POST.get('registerPassword')
         r_password =  request.POST.get('registerRepeatPassword')
-        if password ==r_password:
-            if User.objects.filter(username=username).exists():
-                messages.warning(request,"Username Taken")
-            elif User.objects.filter(email=email).exists():
-                messages.warning(request,"Email Taken")
-            else:
-                user = User.objects.create_user(username=username,password=password,email=email,first_name=name)
-                user.save();
-                messages.success(request,"User Created")
-                return redirect("/")
+        reName = re.compile("[a-zA-Z]")
+        match = re.match(reName,name)
+        if (match is None):
+            messages.warning(request,"For name use only alphabets")
+        elif User.objects.filter(username=username).exists():
+            messages.warning(request,"Username Taken")
+        elif password !=r_password:
+            messages.danger(request,"Password and confirm password didn't match!!!")
+        elif User.objects.filter(email=email).exists():
+            messages.warning(request,"Email Taken")
+        else:
+            user = User.objects.create_user(username=username,password=password,email=email,first_name=name)
+            user.save();
+            messages.success(request,"User Created")
+            return redirect("/")
     return render(request,"register.html")
+
 
 def add(request):
     if request.user.is_anonymous:
@@ -51,10 +58,14 @@ def addjobs(request):
         place = request.POST.get('place')
         phone = request.POST.get('phone')
         email = request.user.email
-        job = NewJob(email=email,jobRole=jobRole,jobDesc=jobDesc,place=place,phone=phone)
-        job.save()
-        messages.success(request,"Job Added")
-        return redirect("/add")
+        rePhone = re.compile("[0-9]")
+        if(re.match(rePhone,phone) is None):
+            messages.warning(request,"Use only number in phone field")
+        else:
+            job = NewJob(email=email,jobRole=jobRole,jobDesc=jobDesc,place=place,phone=phone)
+            job.save()
+            messages.success(request,"Job Added")
+            return redirect("/add")
 
     return render(request,"add_job_opening.html")
 def logoutUser(request):

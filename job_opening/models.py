3cc7b75573680306146acfe7a148975398f4ce07
mayank190821 <bhugramayank@gmail.com>
2022-05-01 10:24:01 +0530

    deleted company model

--- job_opening/models.py
+++ job_opening/models.py
@@ -2,10 +2,11 @@ from ast import Str
 from django.db import models
 from django.forms import CharField, EmailField, EmailInput
 
-class Companie(models.Model):
-    name=models.CharField(max_length=20)
-    username = models.CharField(max_length=20, unique=True)
-    email = models.EmailField(primary_key=True)
-    salt =models.CharField(max_length=100)
+class NewJob(models.Model):
+    email = models.EmailField(max_length=100,primary_key=True)
+    jobRole=models.CharField(max_length=100)
+    jobDesc = models.CharField(max_length=1000)
+    place = models.CharField(max_length=200)
+    phone = models.CharField(max_length=12)
     def __str__(self):
-        return self.username
+        return self.email;

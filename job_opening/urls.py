d04f88bd6e66763995cb615df0a6b011fbcc2cc7
mayank190821 <bhugramayank@gmail.com>
2022-05-02 16:48:48 +0530

    code cleanup

--- job_opening/urls.py
+++ job_opening/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from job_opening import views
 
 urlpatterns = [
-    # path("",views.base,name='base'),
     path("",views.login,name='login'),
     path("register",views.register,name='register'),
     path("add",views.add,name="add"),
